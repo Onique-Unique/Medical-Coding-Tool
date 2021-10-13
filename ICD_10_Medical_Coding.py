a565958e51fa604a41f1324cd2b5fa3eac65ff53
Onique Campbell <87822768+Onique-Unique@users.noreply.github.com>
2021-10-13 13:53:14 -0400

    Update ICD_10_Medical_Coding.py

--- ICD_10_Medical_Coding.py
+++ ICD_10_Medical_Coding.py
@@ -5,21 +5,6 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-#Cholera = {"A00": "Cholera", "A00.0": "Cholera due to Vibrio cholerae 01, biovar cholerae Classical cholera",
-#            "A00.1": "Cholera due to Vibrio cholerae 01, biovar eltor Cholera eltor", "A00.9": "Cholera, unspecified"}
-
-#Typhoid = {"A01": "Typhoid and paratyphoid fevers", "A01.0": "Typhoid fever, Infection due to Salmonella typhi",
-#            "A01.1": "Paratyphoid fever A", "A01.2": "Paratyphoid fever B", "A01.3": "Paratyphoid fever C", 
-#            "A01.4": "Paratyphoid fever, unspecified, Infection due to Salmonella paratyphi NOS"}
-
-#Salmonella = {"A02": "Other - infection or foodborne intoxication due to any Salmonella species other than S. typhi and S. paratyphi",
-#                "A02.0": "Salmonella enteritis, Salmonellosis", "A02.1": "Salmonella sepsis", "A02.2": "Localized salmonella infections",
-#                "A02.8": "Other specified salmonella infections", "A02.9": "Salmonella infection, unspecified"}
-
-#Shigellosis = {"A03": "Shigellosis", "A03.0": "Shigellosis due to Shigella dysenteriae -Group A", "A03.1": "Shigellosis due to Shigella flexneri -Group B",
-#                "A03,2": "Shigellosis due to Shigella boydii -Group C", "A03.3": "Shigellosis due to Shigella sonnei -Group D", "A03.8": "Other shigellosis",
-#                "A03.9": "Shigellosis, unspecified, Bacillary dysentery NOS"}
-
 while True:
     print("Select 1 to find Code Description, 2 to find Code by description or 3 to exit")
     Press = int(input())
